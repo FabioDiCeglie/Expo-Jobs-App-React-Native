c8c784f739ad179408225cf32bdb4cdfc7c7058d
FabioDiCeglie <fabiod@backbase.com>
2023-11-08 19:12:04 +0100

    feat: added some small details

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -22,6 +22,11 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`)
+    setSelectedJob(item.job_id)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -44,7 +49,7 @@ const Popularjobs = () => {
               <PopularJobCard
                 item={item}
                 selectedJob={selectedJob}
-                // handleCardPress={handleCardPress}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={(item) => item?.job_id}
